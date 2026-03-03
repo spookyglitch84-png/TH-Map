c1809d585cee8fa347a42e807ab44848904f77b1
spookyglitch84-png <spookyglitch84@gmail.com>
2026-03-03 10:27:42 +0000

    Fix formatting and structure in fruits.js

--- fruits.js
+++ fruits.js
@@ -2,7 +2,6 @@ const fruitLocations = [
   {
     id: 1,
     coords: [62, 41],
-
     fruits: {
       all: ["Azureberry", "Sunapple", "Ghost Berry"],
       spring: ["Bloodberry", "Emberberry", "Nightshade Berry", "Skydrop"],
@@ -10,12 +9,11 @@ const fruitLocations = [
       fall: ["Amberburst", "Mellowspike", "Mireberry", "Mirthshade", "Scarletip", "Seafallow"],
       winter: ["Faepeach", "Frostgleam", "Icerose", "Lunabright", "Twilipuff", "Velvitfrost"]
     }
-];
+  },
 
   {
     id: 2,
     coords: [75, 52],
-
     fruits: {
       all: ["Sunapple"],
       spring: ["Skydrop"],
