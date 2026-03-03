b12b5febb9ff598f85b775659f00d6c6ba4af5e7
spookyglitch84-png <spookyglitch84@gmail.com>
2026-03-03 10:19:57 +0000

    Add new fruit entry with seasonal data

--- fruits.js
+++ fruits.js
@@ -10,6 +10,18 @@ const fruitLocations = [
       fall: ["Amberburst", "Mellowspike", "Mireberry", "Mirthshade", "Scarletip", "Seafallow"],
       winter: ["Faepeach", "Frostgleam", "Icerose", "Lunabright", "Twilipuff", "Velvitfrost"]
     }
-  }
 ];
 
+  {
+    id: 2,
+    coords: [75, 52],
+
+    fruits: {
+      all: ["Sunapple"],
+      spring: ["Skydrop"],
+      summer: ["Solberry"],
+      fall: [],
+      winter: ["Icerose"]
+    }
+  }
+];
